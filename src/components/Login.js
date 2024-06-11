44d68464af700e798ff2aa86bbb98094011ce001
sk2597 <smruti.karade25@gmail.com>
2024-06-11 22:16:17 +0530

    signin form

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,10 +4,16 @@ const Login = () => {
   return (
     <div>
       <Header />
-      <div>
+      <div className="absolute">
         <img src = "https://assets.nflxext.com/ffe/siteui/vlv3/cacfadb7-c017-4318-85e4-7f46da1cae88/e43aa8b1-ea06-46a5-abe3-df13243e718d/IN-en-20240603-popsignuptwoweeks-perspective_alpha_website_large.jpg"
-        alt = "Background Image" />
+        alt = "Background" />
       </div>
+      <form className="absolute w-3/12 p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
+        <h1 className="font-bold text-3xl py-4">Sign In</h1>
+        <input type="text" placeholder="Email Address" className="p-4 my-4 w-full bg-gray-700"/>
+        <input type="text" placeholder="Password" className="p-4 my-4 w-full bg-gray-700"/>
+        <button className="p-4 my-6 bg-red-700 w-full rounded-lg">Sign In</button>
+      </form>
     </div>
   )
 }
