c4fb5bb6336e6331494ebd82d333e44be19fc5ff
sk2597 <smruti.karade25@gmail.com>
2024-06-11 20:59:53 +0530

    setting up routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import { createBrowserRouter } from 'react-router-dom';
 import Body from './components/Body';
+import Explore from './components/Explore';
+import { RouterProvider } from 'react-router-dom';
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Body />,
+  },
+  {
+    path: "/explore",
+    element: <Explore />,
+  }
+]);
 
 function App() {
   return (
-    <Body />
+    <RouterProvider router={appRouter} />
   );
 }
 
