92b6c1f49174d11f4fb1473bedbf8b7c033b499b
sk2597 <smruti.karade25@gmail.com>
2024-06-11 21:02:30 +0530

    correctly setting up routing within the body component instead of app.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,8 @@
-import { createBrowserRouter } from 'react-router-dom';
 import Body from './components/Body';
-import Explore from './components/Explore';
-import { RouterProvider } from 'react-router-dom';
-
-const appRouter = createBrowserRouter([
-  {
-    path: "/",
-    element: <Body />,
-  },
-  {
-    path: "/explore",
-    element: <Explore />,
-  }
-]);
 
 function App() {
   return (
-    <RouterProvider router={appRouter} />
+    <Body />
   );
 }
 
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import Login from './Login'
 import Explore from './Explore'
+import { createBrowserRouter } from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
 
 const Body = () => {
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <Login />,
+    },
+    {
+      path: "/explore",
+      element: <Explore />,
+    },
+  ]);
   return (
     <div>
-      <Login />
-      <Explore />
+      <RouterProvider router={appRouter} />
     </div>
   )
 }
