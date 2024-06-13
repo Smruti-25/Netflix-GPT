5c1edc0bc44b553dc78e0b4fe9681db4ca14ff8b
sk2597 <smruti.karade25@gmail.com>
2024-06-13 23:32:56 +0530

    fetching now playing movies from tmdb

--- src/components/Explore.js
+++ src/components/Explore.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from './Header'
+import { NOW_PLAYING_URL, TMDB_API_OPTIONS } from '../utils/constants';
 
 const Explore = () => {
+  useEffect(() => {
+    getNowPlayingMovies();
+  },[]);
+
+  const getNowPlayingMovies = async() => {
+    const data = await fetch(NOW_PLAYING_URL, TMDB_API_OPTIONS);
+    const nowPlayingMovies = await data.json();
+    console.log({nowPlayingMovies});
+  }
+
   return (
     <div>
       <Header />
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,9 +3,20 @@ export const LOGO =
 
 export const PROFILE_AVATAR =
   "https://occ-0-3216-2186.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABUSntF8sS3XaxlL4kbgMzXhSHilR2oaXKyypWY_hXouAnTxBxaOigBfg-IX5z8rXMp4XyRdumCARSopncAJ3KG2eC7dwaI0.png?r=64f";
-  
+
 export const PROFILE_AVATAR2 =
   "https://occ-0-3216-2186.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABXYofKdCJceEP7pdxcEZ9wt80GsxEyXIbnG_QM8znksNz3JexvRbDLr0_AcNKr2SJtT-MLr1eCOA-e7xlDHsx4Jmmsi5HL8.png?r=1d4";
 
 export const BACKGROUD_IMAGE =
   "https://assets.nflxext.com/ffe/siteui/vlv3/cacfadb7-c017-4318-85e4-7f46da1cae88/e43aa8b1-ea06-46a5-abe3-df13243e718d/IN-en-20240603-popsignuptwoweeks-perspective_alpha_website_large.jpg";
+
+export const TMDB_API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzOGNkZjlmZjc5NmQ5YjAwYmE5NGI2ZWY1ZTg5OTU3OCIsInN1YiI6IjY2NmIxYmMwNTc5OWY2Y2MwODM2ZmY2OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ExeV3j893YYI69gwGqPZ6vBaPXE1OpsgTY1SvBJkiSI",
+  },
+};
+
+export const NOW_PLAYING_URL ='https://api.themoviedb.org/3/movie/now_playing?page=1';
