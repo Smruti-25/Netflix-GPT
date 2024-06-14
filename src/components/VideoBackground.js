1a06945c0ff3ea2c0d2283b236195e058d8888ce
sk2597 <smruti.karade25@gmail.com>
2024-06-14 16:40:00 +0530

    created a custom hook to getMovieTrailer

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,27 +1,9 @@
-import { useEffect } from "react";
-import { TMDB_API_OPTIONS } from "../utils/constants";
-import { useDispatch, useSelector } from "react-redux";
-import {addTrailerVideo} from '../utils/moviesSlice'
+import { useSelector } from "react-redux";
+import useMovieTrailer from "../hooks/useMovieTrailer";
 
-const VideoBackground = (movieId) => {
+const VideoBackground = ({movieId}) => {
   const trailerVideo = useSelector((store) => store.movies?.trailerVideo);
-  useEffect(() => {
-    getMovieVideos();
-  }, []);
-
-  const dispatch = useDispatch();
-
-  const getMovieVideos = async () => {
-    const data = await fetch(
-      `https://api.themoviedb.org/3/movie/653346/videos`,
-      TMDB_API_OPTIONS
-    );
-
-    const json = await data.json();
-    const filteredData = json.results.filter(video => video.type === "Trailer");
-    const trailer = filteredData.length?filteredData[0]:json.results[0];
-    dispatch(addTrailerVideo(trailer))
-  };
+  useMovieTrailer(movieId);
 
   return (
     <div>
